65b8cb6077daa475079be78c85ec7ac869da69bd
TheLuckyPerson <tylerle122002@gmail.com>
2023-10-01 16:52:20 -0700

    react first app part 2

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,5 +1,5 @@
 // src/MyApp.js
-import React from "react";
+import React, {useState} from 'react';
 import Table from "./Table";
 
 const characters = [
@@ -22,10 +22,26 @@ const characters = [
 ];
   
 function MyApp() {
-    return (
+  const [characters, setCharacters] = useState([
+    {
+      name: 'Charlie',
+      job: 'Janitor',
+    },
+    // the rest of the data
+  ]); 
+
+  function removeOneCharacter (index) {
+      const updated = characters.filter((character, i) => {
+          return i !== index
+      });
+    setCharacters(updated);
+  }
+
+  return (
     <div className="container">
-        <Table characterData={characters} />
-    </div>
-    );
+        <Table characterData={characters} 
+	        removeCharacter={removeOneCharacter} />
+    </div>  
+  )
 }
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -14,10 +14,16 @@ function TableHeader() {
 function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
-            <tr key={index}>
-                <td>{row.name}</td>
-                <td>{row.job}</td>
-            </tr>
+          <tr key={index}>
+            <td>{row.name}</td>
+            <td>{row.job}</td>
+            <td>
+              <button onClick={() => 
+                props.removeCharacter(index)}>
+                Delete
+              </button>
+            </td>
+          </tr>
         );
         }
     );
@@ -28,13 +34,14 @@ function TableBody(props) {
     );
 }
 
-function Table(props) {
-    return (
-      <table>
-        <TableHeader />
-        <TableBody characterData={props.characterData} />
-      </table>
-    );
+function Table (props) {
+  return (
+    <table>
+      <TableHeader />
+      <TableBody characterData={props.characterData} 
+	      removeCharacter={props.removeCharacter} />
+    </table>
+  );
 }
 
 export default Table;
