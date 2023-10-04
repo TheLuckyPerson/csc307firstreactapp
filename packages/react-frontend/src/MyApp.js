437a2b52669e07bcbfa64649224c71cf2cecf2ce
TheLuckyPerson <tylerle122002@gmail.com>
2023-10-03 18:12:43 -0700

    Revert "added submit function to the form"
    This reverts commit 5cfa470d0d75c4e3dfea4a8f836bb42dffa5a2a4.

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -13,15 +13,11 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
-
   return (
     <div className="container">
       <Table characterData={characters} 
         removeCharacter={removeOneCharacter} />
-      <Form handleSubmit={updateList}/>
+      <Form />
     </div>
   )
 }
