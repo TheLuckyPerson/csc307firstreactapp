5e494daf5d58dacc8e78dc30abe22dbe4cc892bd
TheLuckyPerson <tylerle122002@gmail.com>
2023-10-03 22:37:06 -0700

    added user id find

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,7 +34,23 @@ const users = {
     ]
  }
 
- const findUserByName = (name) => { 
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});     
+
+app.get('/users', (req, res) => {
+    res.send(users);
+}); 
+
+const findUserByName = (name) => { 
     return users['users_list']
         .filter( (user) => user['name'] === name); 
 }
@@ -51,16 +67,16 @@ app.get('/users', (req, res) => {
     }
 });
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});     
-
-app.get('/users', (req, res) => {
-    res.send(users);
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+    
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id); 
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
 });
